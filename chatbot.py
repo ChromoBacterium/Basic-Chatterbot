0126f5ca0b725acd4226ac7808f7e3f4a25d1573
ChromoBacterium <34142707+ChromoBacterium@users.noreply.github.com>
2018-11-01 11:01:56 -0400

    Update and rename chatbot.py to random_program.py

--- chatbot.py
+++
@@ -1,8 +0,0 @@
-while True:
-    userInput = input(">>>")
-    if userInput in ['hello','HELLO','Hello','HAI']:
-        print("Hello")
-    if userInput in ['bye']:
-        print("Bye")
-    else:
-        print("I did not understand what you said.")
